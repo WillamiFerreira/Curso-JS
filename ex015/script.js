ddf77cf1628dadae548c256d2092812cce846679
Gabriel Ferreira <willamigabrielferreiradesouza@gmail.com>
2022-12-24 10:05:08 -0300

    incio

---
+++ ex015/script.js
@@ -0,0 +1,10 @@
+function geraTabuada(){
+    let num = window.document.getElementById('txtn')
+    let tab = window.document.getElementById('seltab')
+
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+    } else {
+        let n = Number(num.value)
+    }
+}
