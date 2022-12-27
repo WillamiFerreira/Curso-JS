3031575d3ea3854ddc7beb6145707f1ec8425350
Gabriel Ferreira <willamigabrielferreiradesouza@gmail.com>
2022-12-27 10:46:04 -0300

    exerc    cíio 01 refeito

--- exercícios/ex01/script.js
+++ exercícios/ex01/script.js
@@ -1,19 +1,20 @@
 function carregar(){
-    var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('imagem')
-    var data = new Date()
-    var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas`
+    data = new Date
+    hora = data.getHours()
+    
+    var msg = document.getElementById('msg').innerHTML = `São ${hora} horas`
+    var img = document.getElementById('imagem')
+    
     if (hora >= 0 && hora < 12){
-        img.src='imagens/morning.jpg'
-        document.body.style.background = '#B9E1F8'
-        
-    } else if (hora >= 12 && hora < 18){
+        img.src = 'imagens/morning.jpg'
+        document.body.style.background = '#847BAB'
+    } else if (hora < 18){
         img.src = 'imagens/afternoon.jpg'
-        document.body.style.background = '#FCCF35'
+        document.body.style.background = '#9F660F'
     } else {
         img.src = 'imagens/night.jpg'
-        document.body.style.background = '#882D42'
+        document.body.style.background = '#211F1E'
     }
+
 
 }
