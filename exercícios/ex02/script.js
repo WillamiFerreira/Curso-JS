72c77bfeeab5c2c3d7ea8c40ea8353a27dc248ed
Gabriel Ferreira <willamigabrielferreiradesouza@gmail.com>
2022-12-30 08:13:04 -0300

    exercício 02 terminado

--- exercícios/ex02/script.js
+++ exercícios/ex02/script.js
@@ -1,8 +1,43 @@
 function verificar(){
-    var idade = window.document.getElementById('idade')
+    var data = new Date()
+    var ano = data.getFullYear()
+    var ano_n = window.document.getElementById('ano_nasc')
     var sexo = window.document.getElementsByName('sexo')
     var imagem = document.createElement('img')
-    window.alert(sexo.value)
+    var res = window.document.getElementById('resposta')
+    var idade = (ano - ano_n.value)
+    
+    if (ano_n.value.length == 0){
+        window.alert('Digite uma data de nascimento!')
 
+    } else {
+        if (sexo[0].checked){
+           sexo.value = 'Homem'
+          if (idade > 0 && idade <= 8){
+           imagem.src = 'imagens/boy.jpg'
+          } else if (idade < 22) {
+            imagem.src = 'imagens/young_boy.jpg'
+          } else if (idade < 50){
+            imagem.src = 'imagens/man.jpg'
+          } else {
+            imagem.src = 'imagens/old_man.jpg'
+          }
+            
+        } else {
+            sexo.value = 'Mulher'
+          if (idade > 0 && idade <= 8){
+           imagem.src = 'imagens/girl.jpg'
+          } else if (idade < 22) {
+            imagem.src = 'imagens/young_girl.jpg'
+          } else if (idade < 50){
+            imagem.src = 'imagens/woman.jpg'
+          } else {
+            imagem.src = 'imagens/old_woman.jpg'
+          }
+        }
+        res.innerHTML = `${sexo.value} de ${idade} anos`
+        res.appendChild(imagem)
+       
 
+    } 
 }
