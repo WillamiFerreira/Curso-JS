bf098d116618ea3a4f99d644880237eabbee8ab4
Gabriel Ferreira <willamigabrielferreiradesouza@gmail.com>
2022-12-30 07:15:57 -0300

    pastas criadas

---
+++ exercícios/ex02/script.js
@@ -0,0 +1,8 @@
+function verificar(){
+    var idade = window.document.getElementById('idade')
+    var sexo = window.document.getElementsByName('sexo')
+    var imagem = document.createElement('img')
+    window.alert(sexo.value)
+
+
+}
